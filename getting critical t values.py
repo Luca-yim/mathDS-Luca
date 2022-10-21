0c4655b01e66c1a848728fc25baf421ef481fc73
Luca-yim <82668646+Luca-yim@users.noreply.github.com>
2022-10-21 14:44:18 +0100

    Add files via upload

---
+++ getting critical t values.py
@@ -0,0 +1,10 @@
+from scipy.stats import t
+
+# get critical value range for 95% confidence
+# with a sample size of 25
+
+n = 25
+lower = t.ppf(.025, df=n-1)
+upper = t.ppf(.975, df=n-1)
+
+print(lower, upper)
